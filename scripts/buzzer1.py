e84b2b3696ed0999a9ffc6ae300f0ca368253b72
Kae Doki <doki@aitech.ac.jp>
2020-03-11 09:28:26 +0900

    Add a commnet

--- scripts/buzzer1.py
+++ scripts/buzzer1.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 #encoding:utf-8
+
+#include rospy
 import rospy
+
+#initialization of node, then registered the node into ROS
 rospy.init_node('buzzer')
+
+# keep the node active (for waiting)
 rospy.spin()
